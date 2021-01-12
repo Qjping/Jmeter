80c728698ef2509c43cc679e9cf10286a8d29f42
Qjping <1490665859@qq.com>
2021-01-12 16:42:32 +0800

    jmeter如何创建测试计划

--- src/main/java/com/kafaka/kafakaProducerMsg.java
+++ src/main/java/com/kafaka/kafakaProducerMsg.java
@@ -1,28 +1,28 @@
-package com.kafaka;
-
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-
-import java.util.Properties;
-
-/**
- * 在jmeter里面测试需要把kafka的jar放进去，不需要写main方法
- */
-public class kafakaProducerMsg {
-    public static void main(String[]args){
-        Properties properties=new Properties();
-
-        //制度kafka配置
-
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"IP");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serializable.serialization.StringSerializer");
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serializable.serializableString");
-
-        Producer produce=new KafkaProducer(properties);
-        produce.send(new ProducerRecord("test","1111"));
-        produce.close();
-
-    }
-}
+//package com.kafaka;
+//
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.Producer;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.clients.producer.ProducerRecord;
+//
+//import java.util.Properties;
+//
+///**
+// * 在jmeter里面测试需要把kafka的jar放进去，不需要写main方法
+// */
+//public class kafakaProducerMsg {
+//    public static void main(String[]args){
+//        Properties properties=new Properties();
+//
+//        //制度kafka配置
+//
+//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"IP");
+//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serializable.serialization.StringSerializer");
+//        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serializable.serializableString");
+//
+//        Producer produce=new KafkaProducer(properties);
+//        produce.send(new ProducerRecord("test","1111"));
+//        produce.close();
+//
+//    }
+//}
